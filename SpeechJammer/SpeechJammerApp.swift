efe458b465b5733a9047e09cf633c2976f85225b
Jared Jones <84288718+JaredDanielJones@users.noreply.github.com>
2023-10-02 22:58:11 -0500

    Initial Commit

---
+++ SpeechJammer/SpeechJammerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechJammerApp.swift
+//  SpeechJammer
+//
+//  Created by Jared Jones on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SpeechJammerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
